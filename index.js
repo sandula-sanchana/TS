5c4bf47c009ca2b1049936287afbff90a29fd557
sandula-sanchana <sandulasanchana@gmail.com>
2025-11-19 09:54:33 +0530

    primitive types

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "saNCHA";
+console.log(name);
+let number = 1;
+let is = true;
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+
+let name :String ="saNCHA";
+console.log(name);
+
+let number :number=1;
+let is:boolean=true;
